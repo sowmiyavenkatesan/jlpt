b1bea0ca77a9f15b6b54bb2c014462953c944ccc
sowmiyavenkatesan <46124431+sowmiyavenkatesan@users.noreply.github.com>
2020-05-02 19:54:46 +0530

    Add files via upload

---
+++ download_form.php
@@ -0,0 +1,17 @@
+<?php 
+  
+$file = $_GET["file"] .".pdf"; 
+  
+// We will be outputting a PDF 
+header('Content-Type: application/pdf'); 
+  
+// It will be called downloaded.pdf 
+header('Content-Disposition: attachment; filename="jlpt_form.pdf"'); 
+  
+$imagpdf = file_put_contents($image, file_get_contents($file));  
+  
+echo $imagepdf; 
+?>
+
+
+		
